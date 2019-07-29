23c4e0eba28ba8b24cc15e417913f72df4810973
Ariela Cohen <arielaades@gmail.com>
2019-07-29 20:07:37 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,9 +3,13 @@ array.sort
 end
 
 def sort_array_desc(array)
-  array.sort(a > b)
+  array.sort{|a,b| a<b}
 end
 
 def sort_array_char_count(array)
 array.sort {|a,b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+  
 end
