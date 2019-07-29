12513ca7ede0f82fc3af771523daa381d053ed99
Ariela Cohen <arielaades@gmail.com>
2019-07-29 20:19:11 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,3 +15,13 @@ element_to_remove = array[2]
 array.delete_at(2)
 array.insert(1,element_to_remove)
 end
+
+def swap_elements_from_to(array, index, destination_index)
+element_to_remove = array[index]
+array.delete_at[index]
+array.insert(destination_index, element_to_remove)
+end
+
+def reverse_array(array)
+array.sort { |a,b| -(a<=>b) }
+end
